b0a6a87eae599e711e39a6e827eae218ac687f09
DNeves <neves.dme@gmail.com>
2021-04-27 12:22:00 +0100

    script update

---
+++ analysis_script.R
@@ -0,0 +1,4 @@
+# first, start by reading feature names and activity names
+features <- read.table("./UCI HAR Dataset/features.txt")
+activities <- read.table("./UCI HAR Dataset/activity_labels.txt")
+
